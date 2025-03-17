5214084ac478b62c4bdc354e5a5d33fa1b42e3be
macbook <0422anran@gmail.com>
2025-03-17 17:21:18 +0800

    websocket and disruptor finished

---
+++ src/main/java/com/anran/cloudlibrary/manager/websocket/WsHandshakeInterceptor.java
@@ -0,0 +1,104 @@
+package com.anran.cloudlibrary.manager.websocket;
+
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.StrUtil;
+import com.anran.cloudlibrary.manager.auth.SpaceUserAuthManager;
+import com.anran.cloudlibrary.manager.auth.model.SpaceUserPermissionConstant;
+import com.anran.cloudlibrary.model.entity.Picture;
+import com.anran.cloudlibrary.model.entity.Space;
+import com.anran.cloudlibrary.model.entity.User;
+import com.anran.cloudlibrary.model.enums.SpaceTypeEnum;
+import com.anran.cloudlibrary.service.PictureService;
+import com.anran.cloudlibrary.service.SpaceService;
+import com.anran.cloudlibrary.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 建立连接前需要进行权限校验
+ */
+@Component
+@Slf4j
+public class WsHandshakeInterceptor implements HandshakeInterceptor {
+
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private PictureService pictureService;
+
+    @Resource
+    private SpaceService spaceService;
+
+    @Resource
+    private SpaceUserAuthManager spaceUserAuthManager;
+
+
+    /**
+     * 建立连接前操作
+     */
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest) {
+            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+            // 获取请求参数
+            String pictureId = servletRequest.getParameter("pictureId");
+            if (StrUtil.isBlank(pictureId)) {
+                log.error("缺少图片参数，拒绝握手");
+                return false;
+            }
+            // 获取当前登录用户
+            User loginUser = userService.getLoginUser(servletRequest);
+            if (ObjUtil.isEmpty(loginUser)) {
+                log.error("用户未登录，拒绝握手");
+                return false;
+            }
+            // 校验用户是否有该图片的权限
+            Picture picture = pictureService.getById(pictureId);
+            if (picture == null) {
+                log.error("图片不存在，拒绝握手");
+                return false;
+            }
+            // 如果是团队空间，并且有编辑权限 才能建立连接
+            Long spaceId = picture.getSpaceId();
+            Space space = null;
+            if (spaceId != null) {
+                space = spaceService.getById(spaceId);
+                if (space == null) {
+                    log.error("空间不存在，拒绝握手");
+                    return false;
+                }
+                if (space.getSpaceType() != SpaceTypeEnum.TEAM.getValue()) {
+                    log.info("不是团队空间，拒绝握手");
+                    return false;
+                }
+            }
+            // 项目的拓展性
+            List<String> permissionList = spaceUserAuthManager.getPermissionList(space, loginUser);
+            if (!permissionList.contains(SpaceUserPermissionConstant.PICTURE_EDIT)) {
+                log.error("没有图片编辑权限，拒绝握手");
+                return false;
+            }
+            // 设置用户信息 登录信息等 attributes 到 WebSocket 会话中
+            attributes.put("user", loginUser);
+            attributes.put("userId", loginUser.getId());
+            attributes.put("pictureId", Long.valueOf(pictureId));
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+    }
+}
